b4e742d3101a156376c41476d54086cae48b81e1
jonbakerfish <jonbakerfish@gmail.com>
2014-01-15 02:02:03 +0800

    edit all web.config to normal user

--- WxSimulateWeb/Account/Login.aspx.cs
+++ WxSimulateWeb/Account/Login.aspx.cs
@@ -9,6 +9,6 @@ public partial class Account_Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //RegisterHyperLink.NavigateUrl = "Register.aspx?ReturnUrl=" + HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+        RegisterHyperLink.NavigateUrl = "Register.aspx?ReturnUrl=" + HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
     }
 }
